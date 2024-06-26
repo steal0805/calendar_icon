95414f664784ce817af80ec11488d1b09ed2dc92
steal0805 <161389023+steal0805@users.noreply.github.com>
2024-06-26 13:27:23 -0400

    Update update_icon.sh

--- update_icon.sh
+++ update_icon.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-CURRENT_DATE=$(date +'%Y-%m-%d')
-REALATIVE_DIR="$(dirname "${BASH_SOURCE}")"
-SCRIPT_DIR="$(cd $REALATIVE_DIR && echo "$(pwd)")"
 
-OUTPUT_FILE="${SCRIPT_DIR}/../_attachments/icons/calendar_icon.svg"
+# Check if the argument is provided
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <output_path>"
+  exit 1
+fi
+
+# First argument is the SVG output path
+OUTPUT_FILE="$1"
+
+CURRENT_DATE=$(date +'%Y-%m-%d')
 URL="https://api.wolai.com/v1/icon?type=1&locale=en&color=grey&date=$CURRENT_DATE"
+
+# Download the SVG file from the URL and save it to the specified output path
 wget "$URL" -O "$OUTPUT_FILE"
+
+echo "SVG file downloaded and saved to: $OUTPUT_FILE"
